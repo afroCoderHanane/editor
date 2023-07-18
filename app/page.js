6dfc16288409acd94f2745306490e9a4778e2a4a
Abdoul Hanane Gbadamassi <gbadhanane@gmail.com>
2023-07-18 12:00:36 -0700

    updates

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-// import ImageEditor from '../components/ImageEditorv4';
-import ImageEditor from '../components/ImageEditor';
+import ImageEditor from '../components/ImageEditorv4';
+// import ImageEditor from '../components/ImageEditor';
 export default function Home() {
   const imageURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Mike_Bloomberg_Headshot.jpg/1200px-Mike_Bloomberg_Headshot.jpg";
   return (
--- components/ImageEditorv4.jsx
+++ components/ImageEditorv4.jsx
@@ -1,32 +1,41 @@
 'use client'
+
 import React, { useRef, useState } from 'react';
 import Cropper from 'react-cropper';
-import Slider from 'react-input-slider';
 import 'cropperjs/dist/cropper.css';
 
 function ImageEditor({ imageUrl }) {
   const cropperRef = useRef(null);
   const [cropMode, setCropMode] = useState(false);
-  const [filterMode, setFilterMode] = useState(false);
   const [image, setImage] = useState(imageUrl);
   const [imageHistory, setImageHistory] = useState([imageUrl]);
   const [cropAspectRatio, setCropAspectRatio] = useState(1);
+  const [customCropSize, setCustomCropSize] = useState({width: 200, height: 200}); // new crop size
 
-  const [filterSettings, setFilterSettings] = useState({
-    contrast: 1,
-    saturation: 1,
-    vibrance: 0,
-    exposure: 1,
-  });
+
+  const cropPresets = [
+    { label: '4:3', aspectRatio: 4 / 3 },
+    { label: '16:9', aspectRatio: 16 / 9 },
+    {label: '1:1', aspectRatio: 1},
+    { label: '3:4', aspectRatio: 3 / 4 },
+  ];
+
+  const handleCropPresetClick = (presetAspectRatio) => {
+    if (cropperRef.current) {
+      const imageElement = cropperRef.current;
+      const cropper = imageElement.cropper;
+      cropper.setAspectRatio(presetAspectRatio);
+    }
+    setCropAspectRatio(presetAspectRatio);
+  };
 
   const handleCropClick = () => {
-    setFilterMode(false);
     if (cropperRef.current) {
       const imageElement = cropperRef.current;
       const cropper = imageElement.cropper;
       if (!cropMode) {
         cropper.setDragMode('crop');
-        cropper.setCropBoxData({ left: 0, top: 0, width: 200, height: 200 });
+        cropper.setCropBoxData({ left: 0, top: 0, width: 200, height: 200 / cropAspectRatio });
       } else {
         cropper.setDragMode('move');
       }
@@ -38,14 +47,24 @@ function ImageEditor({ imageUrl }) {
     if (cropperRef.current) {
       const imageElement = cropperRef.current;
       const cropper = imageElement.cropper;
-      const croppedImageUrl = cropper.getCroppedCanvas().toDataURL();
-      setImage(croppedImageUrl);
+      const croppedImageDataUrl = cropper.getCroppedCanvas().toDataURL();
+      setImage(croppedImageDataUrl);
+      setImageHistory(prevHistory => [...prevHistory, croppedImageDataUrl]);
       cropper.setDragMode('move');
       cropper.clear();
-      setImageHistory([...imageHistory, croppedImageUrl]);
     }
     setCropMode(false);
-    setFilterMode(false);
+  };
+
+  const handleUndoClick = () => {
+    if (imageHistory.length > 1) {
+      setImageHistory(prevHistory => {
+        const newHistory = [...prevHistory];
+        newHistory.pop();
+        setImage(newHistory[newHistory.length - 1]);
+        return newHistory;
+      });
+    }
   };
 
   const handleSave = () => {
@@ -55,35 +74,20 @@ function ImageEditor({ imageUrl }) {
     link.click();
   };
 
-  const handleUndo = () => {
-    if (imageHistory.length > 1) {
-      const newHistory = [...imageHistory];
-      newHistory.pop();
-      setImage(newHistory[newHistory.length - 1]);
-      setImageHistory(newHistory);
+  const handleCustomCropSize = () => {
+    if (cropperRef.current) {
+      const imageElement = cropperRef.current;
+      const cropper = imageElement.cropper;
+      cropper.setAspectRatio(NaN); // Ignoring the aspect ratio
+      cropper.setCropBoxData({ left: 0, top: 0, width: customCropSize.width, height: customCropSize.height });
     }
   };
+  
 
   const handleFilterClick = () => {
-    setCropMode(false);
-    setFilterMode(true);
-  };
-
-  // TODO: replace these placeholder functions with actual filter logic
-  const handleContrastChange = (value) => {
-    setFilterSettings({ ...filterSettings, contrast: value });
-  };
-
-  const handleSaturationChange = (value) => {
-    setFilterSettings({ ...filterSettings, saturation: value });
-  };
-
-  const handleVibranceChange = (value) => {
-    setFilterSettings({ ...filterSettings, vibrance: value });
-  };
-
-  const handleExposureChange = (value) => {
-    setFilterSettings({ ...filterSettings, exposure: value });
+    console.log('Filter');
+    // If you implement any filter action, don't forget to update the history as well
+    // setImageHistory(prevHistory => [...prevHistory, newImage]);
   };
 
   return (
@@ -105,81 +109,59 @@ function ImageEditor({ imageUrl }) {
             Filter
           </button>
           <button
+            className={`px-4 py-2 ${!image || imageHistory.length < 2 ? 'bg-gray-300 cursor-not-allowed' : 'bg-red-500 text-white cursor-pointer'} rounded`}
+            disabled={!image || imageHistory.length < 2}
+            onClick={handleUndoClick}
+          >
+            Undo
+          </button>
+          <button
             className={`px-4 py-2 ${!image ? 'bg-gray-300 cursor-not-allowed' : 'bg-yellow-500 text-white cursor-pointer'} rounded`}
             disabled={!image}
             onClick={handleSave}
           >
             Save
           </button>
-          <button
-            className={`px-4 py-2 ${imageHistory.length > 1 ? 'bg-red-500 text-white cursor-pointer' : 'bg-gray-300 cursor-not-allowed'} rounded`}
-            disabled={imageHistory.length <= 1}
-            onClick={handleUndo}
-          >
-            Undo
-          </button>
         </div>
         {cropMode && (
-          <div className="w-full flex flex-row justify-around items-center border border-black rounded p-2 mt-2">
-            <button
-              className="px-4 py-2 bg-green-500 text-white cursor-pointer rounded"
-              onClick={() => setCropAspectRatio(4 / 3)}
-            >
-              4:3
-            </button>
-            <button
-              className="px-4 py-2 bg-green-500 text-white cursor-pointer rounded"
-              onClick={() => setCropAspectRatio(16 / 9)}
-            >
-              16:9
-            </button>
-            <button
-              className={`px-4 py-2 ${!image ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500 text-white cursor-pointer'} rounded`}
-              disabled={!image}
-              onClick={handleApplyClick}
-            >
-              Apply
-            </button>
-          </div>
-        )}
-        {filterMode && (
-          <div className="w-full flex flex-col items-center border border-black rounded p-2 mt-2">
-            <label>Contrast: {filterSettings.contrast}</label>
-            <Slider
-              axis="x"
-              xstep={0.1}
-              xmin={0}
-              xmax={2}
-              x={filterSettings.contrast}
-              onChange={({ x }) => handleContrastChange(x)}
-            />
-            <label>Saturation: {filterSettings.saturation}</label>
-            <Slider
-              axis="x"
-              xstep={0.1}
-              xmin={0}
-              xmax={2}
-              x={filterSettings.saturation}
-              onChange={({ x }) => handleSaturationChange(x)}
-            />
-            <label>Vibrance: {filterSettings.vibrance}</label>
-            <Slider
-              axis="x"
-              xstep={0.1}
-              xmin={0}
-              xmax={2}
-              x={filterSettings.vibrance}
-              onChange={({ x }) => handleVibranceChange(x)}
-            />
-            <label>Exposure: {filterSettings.exposure}</label>
-            <Slider
-              axis="x"
-              xstep={0.1}
-              xmin={0}
-              xmax={2}
-              x={filterSettings.exposure}
-              onChange={({ x }) => handleExposureChange(x)}
-            />
+          <div className="w-full flex justify-around items-center border border-black rounded p-2 mt-2">
+            {cropPresets.map((preset, index) => (
+              <button
+                key={index}
+                className={`px-4 py-2 rounded ${cropAspectRatio === preset.aspectRatio ? 'bg-green-500' : 'bg-gray-500'} text-white cursor-pointer`}
+                onClick={() => handleCropPresetClick(preset.aspectRatio)}
+              >
+                {preset.label}
+              </button>
+            ))}
+             <div>
+               <div>
+               <label style={{color:'black'}}>W</label>
+                <input
+                  type="number"
+                  placeholder="Width"
+                  style={{width: '50px',marginLeft:'5px', marginRight: '10px', color: 'black', border:'1px solid black'}}
+                  value={customCropSize.width}
+                  onChange={(e) => setCustomCropSize({ ...customCropSize, width: parseInt(e.target.value, 10) || 0 })}
+                />
+                <label style={{color:'black', border:'black'}}>H</label>
+                <input
+                  type="number"
+                  style={{width: '50px',marginLeft:'5px', marginRight: '10px', color: 'black', border:'1px solid black'}}
+                  placeholder="Height"
+                  value={customCropSize.height}
+                  onChange={(e) => setCustomCropSize({ ...customCropSize, height: parseInt(e.target.value, 10) || 0 })}
+                />
+                
+                <button
+                  className={`px-4 py-2 ${!image ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500 text-white cursor-pointer'} rounded`}
+                  disabled={!image}
+                  onClick={() => handleCustomCropSize()}
+                >
+                  Set
+                </button>
+                </div>
+              </div>
             <button
               className={`px-4 py-2 ${!image ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-500 text-white cursor-pointer'} rounded`}
               disabled={!image}
@@ -190,16 +172,21 @@ function ImageEditor({ imageUrl }) {
           </div>
         )}
       </div>
-      <div className="flex justify-around w-4/5 flex-grow p-4">
-        <Cropper
-          ref={cropperRef}
-          src={image}
-          style={{ height: '100%', width: '100%' }}
-          initialAspectRatio={cropAspectRatio}
-          aspectRatio={cropAspectRatio}
-          preview=".img-preview"
-          guides={false}
-        />
+      <div className="flex justify-around w-full flex-grow p-4">
+        <div className="w-1/2 relative overflow-hidden flex items-center justify-center bg-white border-4 border-black rounded">
+          {image && (
+            <Cropper
+              src={image}
+              ref={cropperRef}
+              style={{ height: '100%', width: '100%' }}
+              aspectRatio={cropAspectRatio || 1}
+              guides={false}
+              dragMode="move"
+              autoCrop={false}
+              background={true}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
